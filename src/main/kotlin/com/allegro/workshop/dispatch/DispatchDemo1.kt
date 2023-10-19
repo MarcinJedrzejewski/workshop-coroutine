1254b44db19ca3fabdcfd12b088f3756e6b16e8b
Marcin Jedrzejewski <marcin.jedrzejewski@allegro.pl>
2023-10-19 14:30:12 +0200

    dispatch

--- src/main/kotlin/com/allegro/workshop/dispatch/DispatchDemo1.kt
+++ src/main/kotlin/com/allegro/workshop/dispatch/DispatchDemo1.kt
@@ -1,12 +1,29 @@
 package com.allegro.workshop.dispatch
 
 import com.allegro.workshop.callback.User
+import com.allegro.workshop.suspensionpoint.SuspensionPointDemo3
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.intrinsics.COROUTINE_SUSPENDED
+import kotlin.coroutines.intrinsics.suspendCoroutineUninterceptedOrReturn
+
+private val workerPool = Executors.newScheduledThreadPool(1)
 
 
 suspend fun doWork(user: User) {
 
     // use kotlin.coroutines.intrinsics.intercepted
     // TODO copy implementation from SuspensionPointDemo3, use a separated worker pool
+    suspendCoroutineUninterceptedOrReturn { cont ->
+        val work = {
+            println("${Thread.currentThread()} | do some work for User: $user")
+            cont.resumeWith(Result.success(user))
+        }
+        workerPool.schedule(work, 5000, TimeUnit.MILLISECONDS)
+        COROUTINE_SUSPENDED
+    }
 }
 
 class DispatchDemo1 {
@@ -32,5 +49,14 @@ class DispatchDemo1 {
 
 suspend fun main() {
 
-    DispatchDemo1().fetchAndShowUser()
+    val dispatcher = Executors.newScheduledThreadPool(1)
+    val ping = {
+        println("${Thread.currentThread()} | ping ")
+    }
+    dispatcher.scheduleAtFixedRate(ping, 1000, 1000, TimeUnit.MILLISECONDS)
+
+    withContext(dispatcher.asCoroutineDispatcher()) {
+        DispatchDemo1().fetchAndShowUser()
+    }
+
 }
