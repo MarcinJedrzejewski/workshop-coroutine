05b1d99ab243a4f32af9fb9ce1380983b298941e
Marcin Jedrzejewski <marcin.jedrzejewski@allegro.pl>
2023-10-19 14:22:35 +0200

    counter

--- src/main/kotlin/com/allegro/workshop/context/ContextDemo1.kt
+++ src/main/kotlin/com/allegro/workshop/context/ContextDemo1.kt
@@ -7,14 +7,41 @@ import kotlin.coroutines.coroutineContext
 
 class CounterContext(
     private val name: String
-) {
-    // TODO create context element
+) : CoroutineContext.Element {
+
+    override val key: CoroutineContext.Key<*> = Key
+
+    private var counter  = 0
+
+    fun printCounter() {
+        println("$name : $counter")
+        counter++
+    }
+
+    companion object Key : CoroutineContext.Key<CounterContext>
 }
 
 suspend fun printNext() {
    // TODO find counter in coroutine context
+   coroutineContext[CounterContext]?.printCounter()
 }
 
 suspend fun main() {
     // TODO set counter in context
+    withContext(CounterContext("Outer")) {
+        printNext()
+        launch {
+            printNext()
+            launch {
+                printNext()
+            }
+            launch(CounterContext("Inner")) {
+                printNext()
+                launch {
+                    printNext()
+                }
+            }
+        }
+        printNext()
+    }
 }
