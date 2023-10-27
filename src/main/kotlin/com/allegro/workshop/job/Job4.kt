8c18c1f70d8b2c34eb274b02a71f4b70f76715f7
Marcin Jedrzejewski <marcin.jedrzejewski@allegro.pl>
2023-10-27 12:02:13 +0200

    Job

--- src/main/kotlin/com/allegro/workshop/job/Job4.kt
+++ src/main/kotlin/com/allegro/workshop/job/Job4.kt
@@ -1,14 +1,17 @@
 package com.allegro.workshop.job
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.system.measureTimeMillis
 
-private fun work(i: Int) {
-    Thread.sleep(1000)
+private suspend fun work(i: Int) {
+    //Thread.sleep(1000)
+    delay(1000)
     println("Work $i done")
 }
 
@@ -23,14 +26,13 @@ fun main() {
     val time = measureTimeMillis {
         runBlocking {
             for (i in 1..2) {
-                // TODO how to join children?
-                MyScope.launch {
+                MyScope.launch(coroutineContext) {
                     work(i)
                 }
             }
         }
     }
 
-    Thread.sleep(3000)
+    ///Thread.sleep(3000)
     println("Done in $time ms")
 }
